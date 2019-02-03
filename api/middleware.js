3116fc3d3b8dc02f21c10b8ba69653c9b4e1bcaa
Holden B <holden.bucher.92@gmail.com>
2019-02-02 20:19:52 -0600

    remove repeated code

--- api/middleware.js
+++ api/middleware.js
@@ -1,22 +1,6 @@
 const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET;
 
-// module.exports = function(req, res, next) {
-// 	const token = req.headers.authorization;
-// 	if (token) {
-// 		jwt.verify(token, secret, (err, decodedToken) => {
-// 			if (err) {
-// 				res.status(401).json({ error: 'Invalid token' });
-// 			} else {
-// 				req.decodedToken = decodedToken;
-// 				next();
-// 			}
-// 		});
-// 	} else {
-// 		res.status(401).json({ message: 'No token provided' });
-// 	}
-// };
-
 module.exports = {
 	auth: function(req, res, next) {
 		const token = req.headers.authorization;
