d0a186782ff5f9dde4ec14ae992d259d969e29f2
Holden B <holden.bucher.92@gmail.com>
2019-02-04 09:09:46 -0600

    meh

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const server = require('./api/server');
+const { Client } = require('pg');
+
+const client = new Client({
+	connectionString: process.env.DATABASE_URL,
+	ssl: true
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+	if (err) throw err;
+	for (let row of res.rows) {
+		console.log(JSON.stringify(row));
+	}
+	client.end();
+});
 
 const port = process.env.PORT || 4000;
 
