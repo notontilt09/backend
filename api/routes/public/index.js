3847815bc5cd826e79cfafb2e6beb5dc080c0289
Holden B <holden.bucher.92@gmail.com>
2019-02-06 11:01:35 -0600

    public get specific trip route

--- api/routes/public/index.js
+++ api/routes/public/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const { getPublicTrips } = require('../../helpers/tripHelpers');
+const { getPublicTrips, getById } = require('../../helpers/tripHelpers');
 const { getUsers } = require('../../helpers/guideHelpers');
 
 const router = express.Router();
+
+router.get('/trips/:tripId', async (req, res, next) => {
+	const { tripId } = req.params;
+	try {
+		const trip = await getById(tripId);
+		if (!trip) return next({ status: 404, message: 'A trip with that ID does not exist' });
+		res.status(200).json(trip);
+	} catch (err) {
+		next({ message: err }, res);
+	}
+});
 
 router.get('/trips/all', async (req, res, next) => {
 	try {
