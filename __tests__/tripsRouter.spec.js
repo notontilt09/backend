b05c2505fbf51ced3edb44d439f352b1cc4c2dbf
Holden B <holden.bucher.92@gmail.com>
2019-02-07 12:07:14 -0600

    all tests passing once again

--- __tests__/tripsRouter.spec.js
+++ __tests__/tripsRouter.spec.js
@@ -16,9 +16,8 @@ beforeAll(async () => {
 
 describe('Trips router tests (/trips routes)', () => {
 	it('should respond with 200', async () => {
-		let guideId = 1;
 		const response = await request(server)
-			.get(`/user/trips/${guideId}/all`)
+			.get(`/user/trips/all`)
 			.set('authorization', token);
 
 		expect(response.status).toBe(200);
@@ -26,7 +25,7 @@ describe('Trips router tests (/trips routes)', () => {
 	it('should return array of objects', async () => {
 		let guideId = 1;
 		let response = await request(server)
-			.get(`/user/trips/${guideId}/all`)
+			.get(`/user/trips/all`)
 			.set('authorization', token);
 		let expected = await getTripsByUser(guideId);
 
@@ -38,29 +37,26 @@ describe('Trips router tests (/trips routes)', () => {
 		});
 	});
 
-	describe('GET /:guideId/:tripId route', () => {
+	describe('GET /:tripId route', () => {
 		it('it should return 200 status for good request', async () => {
-			const guideId = 1;
 			const tripId = 5;
 			let response = await request(server)
-				.get(`/user/trips/${guideId}/${tripId}`)
+				.get(`/user/trips/${tripId}`)
 				.set('authorization', token);
 
 			expect(response.status).toBe(200);
 		});
 		it('should return 404 status for bad tripId or guideId', async () => {
-			let guideId = 1;
 			let tripId = 50;
 			let response = await request(server)
-				.get(`/user/trips/${guideId}/${tripId}`)
+				.get(`/user/trips/${tripId}`)
 				.set('authorization', token);
 
 			expect(response.status).toBe(404);
 
-			guideId = 9;
 			tripId = 5;
 			esponse = await request(server)
-				.get(`/user/trips/${guideId}/${tripId}`)
+				.get(`/user/trips/${tripId}`)
 				.set('authorization', token);
 
 			expect(response.status).toBe(404);
@@ -69,7 +65,7 @@ describe('Trips router tests (/trips routes)', () => {
 			const guideId = 1;
 			const tripId = 5;
 			let response = await request(server)
-				.get(`/user/trips/${guideId}/${tripId}`)
+				.get(`/user/trips/${tripId}`)
 				.set('authorization', token);
 			let expected = await getTripByIds(tripId, guideId);
 
@@ -81,13 +77,12 @@ describe('Trips router tests (/trips routes)', () => {
 		});
 	});
 
-	describe('PUT /:guideId/:tripId route', () => {
+	describe('PUT /:tripId route', () => {
 		it('should respond with 203 for success', async () => {
-			let guideId = 1;
 			let tripId = 5;
 			let update = { description: 'wow no way' };
 			let response = await request(server)
-				.put(`/user/trips/${guideId}/${tripId}`)
+				.put(`/user/trips/${tripId}`)
 				.set('authorization', token)
 				.send(update);
 
@@ -104,43 +99,29 @@ describe('Trips router tests (/trips routes)', () => {
 
 			expect(response.status).toBe(404);
 		});
-		it('should respond with 400 for no guide/trip not being linked', async () => {
-			let guideId = 3;
-			let tripId = 5;
-			let update = { description: 'wow no way' };
-			let response = await request(server)
-				.put(`/user/trips/${guideId}/${tripId}`)
-				.set('authorization', token)
-				.send(update);
-
-			expect(response.status).toBe(400);
-		});
 		it('should respond with 404 for wrong tripId', async () => {
-			let guideId = 1;
 			let tripId = 333;
 			let update = { description: 'wow no way' };
 			let response = await request(server)
-				.put(`/user/trips/${guideId}/${tripId}`)
+				.put(`/user/trips/${tripId}`)
 				.set('authorization', token)
 				.send(update);
 
 			expect(response.status).toBe(404);
 		});
 		it('should return id of changed trip', async () => {
-			let guideId = 1;
-			let tripId = 5;
+			let tripId = 8;
 			let update = { description: 'wow no way' };
 			let response = await request(server)
-				.put(`/user/trips/${guideId}/${tripId}`)
+				.put(`/user/trips/${tripId}`)
 				.set('authorization', token)
 				.send(update);
 
 			expect(response.body).toBe(1);
 		});
 	});
-	describe('POST /:guideId/create', () => {
+	describe('POST /create', () => {
 		it('should respond with 201 on success', async () => {
-			const guideId = 1;
 			const addTrip = {
 				title: 'testaasdfarossa',
 				description: 'This is a test trip again',
@@ -149,44 +130,26 @@ describe('Trips router tests (/trips routes)', () => {
 				type: 'Backcountry rock-climbing'
 			};
 			let response = await request(server)
-				.post(`/user/trips/${guideId}/create`)
+				.post(`/user/trips/create`)
 				.set('authorization', token)
 				.send(addTrip);
 
 			expect(response.status).toEqual(201);
 		});
-		it('should respond with 400 for bad guideId', async () => {
-			const guideId = 111;
-			const addTrip = {
-				title: 'tesasasfdtaso',
-				description: 'This is a test trip again',
-				designation: 'professional',
-				duration: '3.5 days',
-				type: 'Backcountry rock-climbing'
-			};
-			let response = await request(server)
-				.post(`/user/trips/${guideId}/create`)
-				.set('authorization', token)
-				.send(addTrip);
-
-			expect(response.status).toEqual(400);
-		});
 		it('should respond with 404 for missing req params', async () => {
-			const guideId = 1;
 			const addTrip = {
 				designation: 'professional',
 				duration: '3.5 days',
 				type: 'Backcountry rock-climbing'
 			};
 			let response = await request(server)
-				.post(`/user/trips/${guideId}/create`)
+				.post(`/user/trips/create`)
 				.set('authorization', token)
 				.send(addTrip);
 
 			expect(response.status).toEqual(404);
 		});
 		it('should respond with 400 for wrong designation', async () => {
-			const guideId = 1;
 			const addTrip = {
 				title: 'adsfadsg',
 				description: 'This is a test trip again',
@@ -195,14 +158,13 @@ describe('Trips router tests (/trips routes)', () => {
 				type: 'Backcountry rock-climbing'
 			};
 			let response = await request(server)
-				.post(`/user/trips/${guideId}/create`)
+				.post(`/user/trips/create`)
 				.set('authorization', token)
 				.send(addTrip);
 
 			expect(response.status).toEqual(400);
 		});
 		it('should respond with newly created tripId', async () => {
-			const guideId = 1;
 			const addTrip = {
 				title: 'adfas',
 				description: 'This is a test trip again',
@@ -211,7 +173,7 @@ describe('Trips router tests (/trips routes)', () => {
 				type: 'Backcountry rock-climbing'
 			};
 			let response = await request(server)
-				.post(`/user/trips/${guideId}/create`)
+				.post(`/user/trips/create`)
 				.set('authorization', token)
 				.send(addTrip);
 
--- api/routes/protected/tripsRouter.js
+++ api/routes/protected/tripsRouter.js
@@ -13,12 +13,12 @@ const router = express.Router();
 
 router.param('tripId', async function(req, res, next, tripId) {
 	const trip = await getById(tripId);
-	console.log(trip);
 	if (trip) {
 		req.trip = trip;
 		next();
 	} else {
-		next(new Error('A trip with that ID does not exist'));
+		next({ status: 404, message: 'A trip with that ID does not exist' }, res);
+		// next();
 	}
 });
 
@@ -37,10 +37,12 @@ router.get('/:tripId', async (req, res, next) => {
 	const { trip } = req;
 	const { guide } = req.decodedToken;
 	try {
+		// if (!trip) (next({status: 404, message: "A trip with that ID does not exist"}))
 		if (trip.guide_id !== guide.id)
 			return next({ status: 400, message: 'That trip is not connected to the specified guide ID' });
 		res.status(200).json(trip);
 	} catch (err) {
+		console.log(err);
 		next({ message: err }, res);
 	}
 });
@@ -50,7 +52,7 @@ router.put('/:tripId', typeCoercion, async (req, res, next) => {
 	const { guide } = req.decodedToken;
 	const updates = req.body;
 	try {
-		if (!trip) next({ status: 404, message: 'A trip with that ID does not exist' }, res);
+		// if (!trip) next({ status: 404, message: 'A trip with that ID does not exist' }, res);
 		if (trip.guide_id !== guide.id) {
 			next({ status: 400, message: "You must be the trip's guide to make changes" }, res);
 		}
@@ -65,7 +67,7 @@ router.post('/:tripId/upload', async (req, res, next) => {
 	const { trip } = req;
 	const image = req.body;
 	try {
-		if (!trip) return next({ status: 404, message: 'A trip with that id does not exist' }, res);
+		// if (!trip) return next({ status: 404, message: 'A trip with that id does not exist' }, res);
 		const id = await addImage({ ...image, trip_id: trip.id });
 		res.status(201).json(id);
 	} catch (err) {
